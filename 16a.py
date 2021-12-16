05bfa2291ad79ab5f34f3758561e80b14863070f
marktimmerman-dev <72545155+marktimmerman-dev@users.noreply.github.com>
2021-12-16 11:32:16 +0100

    16

--- 16a.py
+++ 16a.py
@@ -1,7 +1,6 @@
 def hex2bin(s):
   return "".join([ ('000'+format(int(c,16),'b'))[-4:] for c in s ])
 
-
 hexdata = open('input.txt').read().splitlines()
 
 packet_versions = []
@@ -11,12 +10,10 @@ def parse(data):
 
   packet_version = int(data[i:i+3],2)
   i += 3
-  print('packet_version', packet_version)
   packet_versions.append(packet_version)
 
   packet_type = int(data[i:i+3],2)
   i += 3
-  print('packet_type', packet_type)
 
   if packet_type == 4:  # litteral value
     litteral = ""
@@ -27,7 +24,6 @@ def parse(data):
       i += 4
       if not not_the_last_group:
         break;
-    print('litteral', litteral, int(litteral, 2))
     return i, int(litteral, 2)
 
   else:
@@ -40,26 +36,16 @@ def parse(data):
       total_length = int(data[i:i+15],2)
       i += 15
 
-      print('operator packet with length in bits',total_length)
-
       j = 0
-      
       while j < total_length:
         k, val = parse(data[i:])
         i += k
         j += k
         r.append(val)
-        
-      if j != total_length:
-        print('!!!!!!!!!!!!! hier is wat geks aan de hand')
-        
-      
 
     else:
       nsubpackets = int(data[i:i+11],2)
       i += 11
-
-      print('operator packet with number of subpackets',nsubpackets)
 
       for j in range(nsubpackets):
         k, val = parse(data[i:])
@@ -70,14 +56,7 @@ def parse(data):
     return i, sum(r)
 
 for h in hexdata:
-  print('-------------------',h,'------------')
   data = hex2bin(h)
-
-  print(data)
-
   packet_versions = []
-
-
-  print(parse(data))
-  print(packet_versions)
+  parse(data)
   print(sum(packet_versions))
--- 16b.py
+++ 16b.py
@@ -3,22 +3,16 @@ import numpy as np
 def hex2bin(s):
   return "".join([ ('000'+format(int(c,16),'b'))[-4:] for c in s ])
 
-
 hexdata = open('input.txt').read().splitlines()
-
-packet_versions = []
 
 def parse(data):
   i = 0
 
   packet_version = int(data[i:i+3],2)
   i += 3
-  print('packet_version', packet_version)
-  packet_versions.append(packet_version)
 
   packet_type = int(data[i:i+3],2)
   i += 3
-  print('packet_type', packet_type)
 
   if packet_type == 4:  # litteral value
     litteral = ""
@@ -29,7 +23,6 @@ def parse(data):
       i += 4
       if not not_the_last_group:
         break;
-    print('litteral', litteral, int(litteral, 2))
     return i, int(litteral, 2)
 
   else:
@@ -42,33 +35,22 @@ def parse(data):
       total_length = int(data[i:i+15],2)
       i += 15
 
-      print('operator packet with length in bits',total_length)
-
       j = 0
-      
       while j < total_length:
         k, val = parse(data[i:])
         i += k
         j += k
         r.append(val)
-        
-      if j != total_length:
-        print('!!!!!!!!!!!!! hier is wat geks aan de hand')
-        
-      
 
     else:
       nsubpackets = int(data[i:i+11],2)
       i += 11
-
-      print('operator packet with number of subpackets',nsubpackets)
 
       for j in range(nsubpackets):
         k, val = parse(data[i:])
         i += k
         r.append(val)
 
-    print('r',r)
     if packet_type == 1:
       return i, np.prod(r)
     if packet_type == 2:
@@ -84,13 +66,6 @@ def parse(data):
     return i, sum(r)
 
 for h in hexdata:
-  print('-------------------',h,'------------')
   data = hex2bin(h)
-
-  print(data)
-
-  packet_versions = []
-
   l, val = parse(data)
-
   print(val)
